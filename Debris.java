13eebf730b3fbf9d3a2ca32c23542cf9ef1df63d
JosethValdez <jsvaldez2@miners.utep.edu>
2025-04-21 18:33:25 -0700

    Update Debris.java

--- Debris.java
+++ Debris.java
@@ -8,7 +8,50 @@
 public class Debris extends SpaceObject{
 
     /** True if this debris has sister debris. */
-    private boolean hasSisterDebris;
+    protected boolean hasSisterDebris;
+
+
+    public Debris(
+            int recordId,
+            int noradCatId,
+            String satelliteName,
+            String country,
+            String orbitType,
+            String objectType,
+            int launchYear,
+            String launchSite,
+            double longitude,
+            double avgLongitude,
+            double[] geohash,
+            boolean isNominated,
+            boolean hasDossier,
+            int daysOld,
+            int conjunctionCount,
+            boolean isUnkObject,
+            boolean hasSisterDebris
+    ) {
+        this.recordId = recordId;
+        this.noradCatId = noradCatId;
+        this.satelliteName = satelliteName;
+        this.country = country;
+        this.orbitType = orbitType;
+        this.objectType = objectType;
+        this.launchYear = launchYear;
+        this.launchSite = launchSite;
+        this.longitude = longitude;
+        this.avgLongitude = avgLongitude;
+        if (geohash != null && geohash.length == 2) {
+            this.geohash[0] = geohash[0];
+            this.geohash[1] = geohash[1];
+        }
+        this.isNominated = isNominated;
+        this.hasDossier = hasDossier;
+        this.daysOld = daysOld;
+        this.conjunctionCount = conjunctionCount;
+        this.isUnkObject = isUnkObject;
+        this.hasSisterDebris = hasSisterDebris;
+    }
+
 
     /**
      * Sets whether this debris has a sister debris.
@@ -26,5 +69,23 @@ public class Debris extends SpaceObject{
      */
     public boolean hasSisterDebris() {
         return hasSisterDebris;
+    }
+
+    public static void assessOrbitSystem(Debris[] debrisArray) {
+        for (Debris debris : debrisArray) {
+            if (debris != null) {
+                System.out.println("Record ID: " + debris.getRecordId());
+                System.out.println("Satellite Name: " + debris.getSatelliteName());
+                System.out.println("Country: " + debris.getCountry());
+                System.out.println("Orbit Type: " + debris.getOrbitType());
+                System.out.println("Launch Year: " + debris.getLaunchYear());
+                System.out.println("Launch Site: " + debris.getLaunchSite());
+                System.out.println("Longitude: " + debris.getLongitude());
+                System.out.println("Avg. Longitude: " + debris.getAvgLongitude());
+                System.out.println("Geohash: [" + debris.getGeohash()[0] + ", " + debris.getGeohash()[1] + "]");
+                System.out.println("Days Old: " + debris.getDaysOld());
+                System.out.println("----------------------------------------------------");
+            }
+        }
     }
 }
